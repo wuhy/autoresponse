f783bcff01e19ecda32680b9c536dfa526f28205
wuhy <sparklewhy@gmail.com>
2015-08-19 15:23:38 +0800

    add faker mock helper method

--- examples/mock/test/notfound.js
+++ examples/mock/test/notfound.js
@@ -1,7 +1,7 @@
 module.exports = function (path, queryParam, postParam) {
     console.log('path: %s, query: %s, post: %s',
         path, JSON.stringify(queryParam || {}), JSON.stringify(postParam || {}));
-    // console.log('global mock helper: ' + mock);
+    console.log('global mock helper: random name = ' + mock.faker().name.findName());
     return {
         status: 0,
         timeout: 0,
